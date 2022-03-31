585bdeeb47cf3eac43202c1c575d99a2b22018ad
cif95 <vivianacifarelli9@gmail.com>
2022-03-31 15:03:07 +0200

    created album cards from api

--- src/components/AlbumCard.vue
+++ src/components/AlbumCard.vue
@@ -1,16 +1,12 @@
 <template>
-  <div class="col-6 col-md-4 col-lg-2">
+  <div class="col">
     <div class="my-album-card">
-      <img
-        class="img-fluid"
-        src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg"
-        alt=""
-      />
-      <h5 class="my-card-title text-white text-uppercase pt-3 fw-bold">
-        New jersey
+      <img class="img-fluid" :src="albumObj.poster" :alt="albumObj.title" />
+      <h5 class="my-card-title">
+        {{ albumObj.title }}
       </h5>
-      <p class="my-card-text">Bon Jovi</p>
-      <span class="my-card-text">1988</span>
+      <p class="my-card-text">{{ albumObj.author }}</p>
+      <span class="my-card-text">{{ albumObj.year }}</span>
     </div>
   </div>
 </template>
@@ -18,18 +14,15 @@
 <script>
 export default {
   name: "AlbumCard",
+  props: {
+    albumObj: Object,
+  },
 };
 </script>
 
 <style scoped lang="scss">
 @import "../assets/scss/partials/_variables.scss";
 div.my-album-card {
-  color: grey;
-  padding: 1.2rem 1.2rem 3.4rem;
-  text-align: center;
-  background-color: $lightGray;
-  p {
-    margin-bottom: 0;
-  }
+  @include albumCard;
 }
 </style>
--- src/components/Albums.vue
+++ src/components/Albums.vue
@@ -1,8 +1,14 @@
 <template>
-  <main>
-    <div class="container-fluid">
-      <div class="row p-5">
-        <AlbumCard />
+  <main class="p-4">
+    <div class="container">
+      <div
+        class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 p-5 gx-5"
+      >
+        <AlbumCard
+          :albumObj="album"
+          v-for="(album, index) in albums"
+          :key="index"
+        />
       </div>
     </div>
   </main>
@@ -10,19 +16,39 @@
 
 <script>
 import AlbumCard from "./AlbumCard.vue";
+import axios from "axios";
 
 export default {
   name: "IndexAlbums",
+  data() {
+    return {
+      albums: [],
+    };
+  },
   components: {
     AlbumCard,
+  },
+  methods: {
+    getAlbums() {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/array/music")
+        .then((result) => {
+          // console.log(result.data.response);
+          this.albums = result.data.response;
+          console.log(this.albums);
+        })
+        .catch((error) => console.error(error));
+    },
+  },
+  created() {
+    this.getAlbums();
   },
 };
 </script>
 
 <style scoped lang="scss">
 @import "../assets/scss/partials/_variables.scss";
-div.container-fluid {
-  height: 92vh;
+main {
   background-color: $darkBlueGray;
 }
 </style>
