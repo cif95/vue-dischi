2e76171f9011a3987ec8c22b309d2d5ca68c273d
cif95 <vivianacifarelli9@gmail.com>
2022-03-31 15:41:39 +0200

    created select

--- src/components/Albums.vue
+++ src/components/Albums.vue
@@ -1,6 +1,20 @@
 <template>
   <main class="p-4">
     <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-5">
+          <select class="form-select" @change="onChange">
+            <option selected>Choose a genre</option>
+            <option
+              v-for="(genre, index) in albumsGenres"
+              :key="index"
+              :value="genre"
+            >
+              {{ genre }}
+            </option>
+          </select>
+        </div>
+      </div>
       <div
         class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 p-5 gx-5"
       >
@@ -23,6 +37,7 @@ export default {
   data() {
     return {
       albums: [],
+      albumsGenres: [],
     };
   },
   components: {
@@ -33,11 +48,19 @@ export default {
       axios
         .get("https://flynn.boolean.careers/exercises/api/array/music")
         .then((result) => {
-          // console.log(result.data.response);
           this.albums = result.data.response;
           console.log(this.albums);
+          this.albums.forEach((element) => {
+            if (!this.albumsGenres.includes(element.genre)) {
+              this.albumsGenres.push(element.genre);
+            }
+          });
+          console.warn(this.albumsGenres);
         })
         .catch((error) => console.error(error));
+    },
+    onChange(event) {
+      console.log(event.target.value);
     },
   },
   created() {
